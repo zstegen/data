2c1de3d5ada9aaea9a0ea3a51e60abec42e7f1eb
zstegen <75817521+zstegen@users.noreply.github.com>
2020-12-16 20:30:56 -0500

    added and modified bfrn1.py to evalute Blotto game.

--- riddler-castles/bfrn1.py
+++ riddler-castles/bfrn1.py
@@ -7,6 +7,23 @@ Created on Mon Dec 14 18:34:04 2020
 
 import csv
 
+def throwdown(generalA,generalB):
+    pointsA = []
+    pointsB = []
+    for castle in range(len(generalA)):
+        if generalA[castle] > generalB[castle]:
+            pointsA.append(castle+1)
+            pointsB.append(0)
+        elif generalA[castle] < generalB[castle]:
+            pointsA.append(0)
+            pointsB.append(castle+1)
+        elif generalA[castle] == generalB[castle]:
+            pointsA.append(0.5*(castle+1))
+            pointsB.append(0.5*(castle+1))
+        else:
+            print('unexpected case!!!')
+    return (pointsA,pointsB)
+    
 allGenerals = []
 
 with open('castle-solutions-ZS-1.csv') as csvFile:
@@ -14,7 +31,8 @@ with open('castle-solutions-ZS-1.csv') as csvFile:
     next(reader)
     for row in reader:
         allGenerals.append([int(s) for s in row])
-    
+
+
 
 
         
